fac9e3187f8e6754072b1b5ff76ef947a56daf2e
Tristian Azuara <tristian.azuara@gmail.com>
2019-07-02 23:22:50 -0700

    Initial commit; template setup

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := """elevate"""
+organization := "com.github"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.13.0"
+
+libraryDependencies += guice
---
+++ target/scala-2.13/routes/main/controllers/routes.java
@@ -0,0 +1,20 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/Tristian/Repositories/github.com/valued-co/dev-triztian/conf/routes
+// @DATE:Tue Jul 02 23:00:31 PDT 2019
+
+package controllers;
+
+import router.RoutesPrefix;
+
+public class routes {
+  
+  public static final controllers.ReverseHomeController HomeController = new controllers.ReverseHomeController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets(RoutesPrefix.byNamePrefix());
+
+  public static class javascript {
+    
+    public static final controllers.javascript.ReverseHomeController HomeController = new controllers.javascript.ReverseHomeController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets(RoutesPrefix.byNamePrefix());
+  }
+
+}
