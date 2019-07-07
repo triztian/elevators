cd0d494c846e084d28f43f9483719b9b8160f2f8
Tristian Azuara <tristian.azuara@gmail.com>
2019-07-07 12:34:15 -0700

    Implement call button click logic

--- public/javascripts/src/CallButton.js
+++ public/javascripts/src/CallButton.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export default function CallButton() {
+export default function CallButton(props) {
+
+	const onClick = (evt) => {
+		props.onCallElevator(evt.target.value);
+	}
+
 	return (
 		<div className="CallButton">
-			<button type="buffon">Call</button>
+			<button 
+				type="button" 
+				value={props.floorNumber}
+				onClick={onClick}
+			>Call</button>
 		</div>
 	)
 }
