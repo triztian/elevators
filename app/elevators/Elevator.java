359c6305f5550476401fcf381e02ad10f112db8b
Tristian Azuara <tristian.azuara@gmail.com>
2019-07-06 23:42:17 -0700

    Add helper methods to Elevator.java

--- app/elevators/Elevator.java
+++ app/elevators/Elevator.java
@@ -18,13 +18,15 @@ public final class Elevator {
 
 	private Queue<Floor> floorQueue = new PriorityBlockingQueue<>();
 
+	private Floor currentFloor = Floor.GROUND;
+
 	private Floor targetFloor = null;
 
-	private Floor currentFloor = null;
 
 	public Elevator() {
 		this.driveSystem = new DriveSystem(this.new MoveTask());
 	}
+
 
 	/**
 	 * Moves the elevator to the next in the direction of the target floor.
@@ -69,13 +71,27 @@ public final class Elevator {
 	/**
 	 * Determine the elevator's traveling direction.
 	 */
-	private synchronized Direction getDirection() {
+	public synchronized Direction getDirection() {
 
 		if (currentFloor.isBelow(targetFloor)) return Direction.UP;
 
 		if (currentFloor.isAbove(targetFloor)) return Direction.DOWN;
 
 		return Direction.NONE;
+	}
+
+	/**
+	 * Obtain the current flor.
+	 */
+	public synchronized Floor getCurrentFloor() {
+		return currentFloor;
+	}
+
+	/**
+	 * OObtain the destination floor; if any.
+	 */
+	public synchronized Floor getDestinationFloor() {
+		return targetFloor;
 	}
 
 	/**
