e5d0ce99f52b6abdf34b210744f5def5330bb0ae
Tristian Azuara <tristian.azuara@gmail.com>
2019-07-07 12:34:56 -0700

    Implement FloorSelector select dest floor logic

--- public/javascripts/src/FloorSelector.js
+++ public/javascripts/src/FloorSelector.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function FloorSelector() {
+export default function FloorSelector(props) {
+
+	const [selectedFloor, setSelectedFloor] = useState(props.selectedFloor);
+
+	const onChange = e => {
+		setSelectedFloor(e.target.value);
+		props.onSelectFloor(e.target.value);
+	}
+
+	let floorOptions = props.floors.map(f => {
+		return (<option key={`fo${f.number}`} value={f.number}>{f.name}</option>);
+	});
+
 	return (
 		<div className="FloorSelector">
-			<select></select>
+			<select onChange={onChange} value={selectedFloor}>
+				{floorOptions}
+			</select>
 		</div>
-	)
+	);
 }
