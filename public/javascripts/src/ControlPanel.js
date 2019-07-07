915200b1fcae21d0c0868d1aa776887ce0a2d2b3
Tristian Azuara <tristian.azuara@gmail.com>
2019-07-07 12:34:35 -0700

    Implement control panel call elevator logic

--- public/javascripts/src/ControlPanel.js
+++ public/javascripts/src/ControlPanel.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import CallButton from './CallButton'
 import FloorSelector from './FloorSelector'
 
+import ElevatorAPI from './ElevatorAPI/Api'
+
 export default function ControlPanel(props) {
+
+	// let api = new ElevatorAPI()
+
+	const [destFloor, setDestFloor] = useState(null);
+
+	const onCallElevator = () => {
+
+		let from = props.floorNumber
+		let to = destFloor;
+
+		console.log('Elevator called: from: ', from, 'to: ', to)
+
+		ElevatorAPI.callElevator(from, to).then((resp) => {
+			console.log('onCallElevator: success: ', resp);
+		}).catch(err => {
+			console.error('onCallElevator: ', err);
+		});
+
+	}
+
 	return (
 		<div className="ControlPanel">
 			<FloorSelector 
 				floors={props.floors}
-
-				/> 
-			<CallButton />
+				selectedFloor={0}
+				onSelectFloor={setDestFloor}
+			/> 
+			<CallButton 
+				floorNumber={props.floorNumber} 
+				onCallElevator={onCallElevator}
+			/>
 		</div>
 	)
 }
