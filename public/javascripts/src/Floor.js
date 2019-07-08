3a7ee7b4685509d884364125d09a39efb9b00ed9
Tristian Azuara <tristian.azuara@gmail.com>
2019-07-07 18:13:11 -0700

    Update Floor css/js

--- public/javascripts/src/Floor.js
+++ public/javascripts/src/Floor.js
@@ -1,13 +1,19 @@
 import React from 'react'
+
 import ControlPanel from "./ControlPanel";
 
+import './Floor.css'
+
 export default function Floor(props) {
+
 	return (
 		<div className="Floor">
+			<div className="Floor-Name">
+				<h4>{props.floorName}</h4>
+			</div>
 			<ControlPanel 
 				floors={props.floors}
-				floorNumber={props.floorNumber}
-			/>
+				floorNumber={props.floorNumber}/>
 			<div></div>
 			<div></div>
 			<div></div>
