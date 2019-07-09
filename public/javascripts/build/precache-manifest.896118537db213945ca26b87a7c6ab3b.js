c4f5ded751c92146cd2757ec59c499072d91c99e
Tristian Azuara <tristian.azuara@gmail.com>
2019-07-09 03:54:31 -0700

    Generate react dist

---
+++ public/javascripts/build/precache-manifest.896118537db213945ca26b87a7c6ab3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c1216db1ca3e33ae9ad047f4e70bc6",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "6068c1a757fac7756e5e",
+    "url": "/app/static/css/main.09c17545.chunk.css"
+  },
+  {
+    "revision": "b2cf1d99ac141ec416bb",
+    "url": "/app/static/js/2.15d68683.chunk.js"
+  },
+  {
+    "revision": "6068c1a757fac7756e5e",
+    "url": "/app/static/js/main.a46ef47b.chunk.js"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  }
+]);
