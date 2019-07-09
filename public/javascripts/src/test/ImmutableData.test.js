2b5d653d35cab6b995190f25e1c70eb8877c0087
Tristian Azuara <tristian.azuara@gmail.com>
2019-07-08 19:56:46 -0700

    Implement Javascript ImmutableData and test

--- public/javascripts/src/test/ImmutableData.test.js
+++ public/javascripts/src/test/ImmutableData.test.js
@@ -1,6 +1,45 @@
-import ImmutableData from '../src/util/ImmutableData'
+import ImmutableData from '../util/ImmutableData'
 
-it('', () => {
+it('ImmutableData should preserve values', () => {
+
 	const srcNumbers = [1,2,3];
-	const data = new ImmutableData(srcNumbers);
+	const srcObj = {
+		a: {
+			x: 99,
+			y: -123,
+			z: {
+				q: 'qstring'
+			}
+		},
+		b: 'a string'
+	}
+	const srcDate = new Date();
+	const srcHours = srcDate.getHours();
+
+	const data = new ImmutableData(srcNumbers, srcObj, srcDate);
+
+	// modify a numbers entry
+	expect(() => {
+		const resNumbers = data.numbers;
+		resNumbers[2] = 99;
+	}).toThrow();
+	expect(srcNumbers[2]).toEqual(3);
+
+	// modify the nested object
+	expect(() => {
+		const resObj = data.data;
+		resObj.a.x = 300;
+	}).toThrow();
+	expect(srcObj.a.x).toEqual(99);
+
+	// modify the nested object
+	expect(() => {
+		const resObj = data.data;
+		resObj.a.z.q = 999
+	}).toThrow();
+	expect(srcObj.a.z.q).toEqual('qstring');
+
+	const resDate = data.date;
+	resDate.setHours(1);
+	expect(srcHours).toEqual(srcDate.getHours());
 })
--- public/javascripts/src/util/ImmutableData.js
+++ public/javascripts/src/util/ImmutableData.js
@@ -16,39 +16,60 @@ export default class ImmutableData {
 	 */
 	constructor(numbers, obj, date) {
 
-		this.numbers = numbers.map(x => x); // copy using the identity function
-		this.obj = obj;
-		this.date = date;
+		this._numbers = numbers.map(x => x); // copy using the identity function
+		this._obj = obj;
+		this._date = date;
 
 		if (Object.freeze) {
-			ImmutableData.deepFreeze(this.numbers);
-			ImmutableData.deepFreeze(this.obj);
-			ImmutableData.deepFreeze(this.date);
+			ImmutableData.deepFreeze(this._numbers);
+			ImmutableData.deepFreeze(this._obj);
 		}
 
 	}
 
 	get numbers() {
-		return this.numbers;
+		return this._numbers;
 	}
 
 	get data() {
-		return this.obj;
+		return this._obj;
 	}
 
 	get date() {
-		return this.date;
+		return new Date(this._date.valueOf());
 	}
 
+	/**
+	 * Recursively freezes the given object's properties, when an object 
+	 * is frozen an exception will be thrown if an property is modified.
+	 * @param {any} obj The object to be deeply freezed.
+	 */
 	static deepFreeze(obj) {
-		for(let p in Object.getOwnPropertyNames(obj)) {
+
+		// console.log('deepFreeze: ', obj);
+
+		if (obj === null) return;
+		if (obj === undefined) return;
+
+		for (let p of Object.keys(obj)) {
+
+			// console.log('deepFreeze: prop: ', p, "object: ", obj[p])
+
 			if (Array.isArray(obj[p])) {
-				obj[p].map(ImmutableData.deepFreeze);
+				for(let i = 0; i < obj[p].length; i++)
+					ImmutableData.deepFreeze(obj[p][i]);
 				continue;
 			}
 
-			ImmutableData.deepFreeze(obj[p]);
+			if (obj[p] === Object(obj[p])) {
+				ImmutableData.deepFreeze(obj[p]);
+			}
+
+			Object.freeze(obj[p]);
 		}
+
+		Object.freeze(obj);
+
 	}
 
 }
